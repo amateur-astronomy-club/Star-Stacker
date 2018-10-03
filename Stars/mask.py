6d55770b41ffcc14fa263a50dec106008e83f932
Ashwin Nair <ashwinnair2610@gmail.com>
2018-10-03 22:05:12 +0530

    Added mask.py

--- Stars/mask.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-img = cv2.imread('img1.jpeg',0)
-resized_image = cv2.resize(img, (100, 50))
-print (np.shape(resized_image))
-cv2.imshow('img',resized_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
---
+++ mask.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+from glob import glob
+from math import fabs, sqrt
+
+images = glob('Stars/*.jpeg')
+np.set_printoptions(threshold=np.nan)
+
+image_list = [cv2.imread(i) for i in images]
+
+for image in image_list:
+    img = np.sum(image, axis = 2)
+    padx = 51 - (np.shape(img)[1]%51)
+    pady = 51 - (np.shape(img)[0]%51)
+
+    img = np.pad(img, ((0,pady), (0,padx)), 'median')
+    print (np.shape(img))
+
+    for i in range(0, np.shape(img)[1], 51):
+        for j in range(0, np.shape(img)[0], 51):
+            inp = img[i:i+51, j:j+51]
+
+            print (np.shape(inp))
+            avg = np.mean(inp)
+            var = np.var(inp)
+
+            out = inp - avg
+
+            mask = np.less(np.absolute(out), 2*np.ones((51,51))*sqrt(var)) + 0
+            # print (np.shape(mask))
