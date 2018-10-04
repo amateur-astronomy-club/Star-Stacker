2e65c3702f55a8f64967c9404fc21a9bcbb89d53
Ashwin Nair <ashwinnair2610@gmail.com>
2018-10-04 19:12:21 +0530

    Changes to mask.py

--- mask.py
+++ mask.py
@@ -10,12 +10,14 @@ image_list = [cv2.imread(i) for i in images]
 
 for image in image_list:
     img = np.sum(image, axis = 2)
+    img
     padx = 51 - (np.shape(img)[1]%51)
     pady = 51 - (np.shape(img)[0]%51)
 
     img = np.pad(img, ((0,pady), (0,padx)), 'median')
 
     count = 0
+    mask = np.zeros((np.shape(img)[0], np.shape(img)[1]))
     for i in range(0, np.shape(img)[0], 51): #upto 3009
         for j in range(0, np.shape(img)[1], 51): #upto 3978
             inp = img[i:i+51, j:j+51]
@@ -26,5 +28,13 @@ for image in image_list:
 
             out = inp - avg
 
-            mask = np.less(np.absolute(out), 2*np.ones((51,51))*sqrt(var)) + 0
-            # print (np.shape(mask))
+            window_mask = np.less(np.absolute(out), 2*np.ones((51,51))*sqrt(var)) + 0
+
+            mask[i:i+51, j:j+51] = window_mask
+
+
+
+
+cv2.imshow("Mask", mask) #shows mask only for last image
+cv2.waitKey(0)
+cv2.destroyAllWindows()
