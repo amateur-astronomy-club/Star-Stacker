c02543b49cf7ff7331792e69352527c604063351
Ashwin Nair <ashwinnair2610@gmail.com>
2018-10-10 00:35:12 +0530

    Help pls

--- extract.py
+++ extract.py
@@ -1,72 +1,76 @@
-# import cv2
-# import numpy as np
-# from glob import glob
- 
-
-# images = glob('Stars/out*.jpeg')
-
-# image_list = [cv2.imread(i) for i in images]
-
-
-# Read image
-# im = cv2.imread("image_list[0]", cv2.IMREAD_GRAYSCALE)
-
-# im = cv2.imread('Stars/out1.jpeg',0)
- 
-# # Set up the detector with default parameters.
-# detector = cv2.SimpleBlobDetector()
- 
-# # Detect blobs.
-# keypoints = detector.detect(im)
- 
-# # Draw detected blobs as red circles.
-# # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-# im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
- 
-# # Show keypoints
-# cv2.imshow("Keypoints", im_with_keypoints)
-# cv2.waitKey(0)
-
 import cv2
+import numpy as np
+from glob import glob
 from math import sqrt
 from skimage import data
 from skimage.feature import blob_dog, blob_log, blob_doh
 from skimage.color import rgb2gray
+from scipy import optimize
 
-import matplotlib.pyplot as plt
+images = glob('Stars/out*.jpeg')
+
+image_list = [cv2.imread(i) for i in images]
+
+np.set_printoptions(threshold=np.nan)
 
 
-# image = data.hubble_deep_field()[0:500, 0:500]
-image = cv2.imread('Stars/out1.jpeg')
-image_gray = rgb2gray(image)
+def gaussian(height, center_x, center_y, width_x, width_y):
+    """Returns a gaussian function with the given parameters"""
+    width_x = float(width_x)
+    width_y = float(width_y)
+    return lambda x,y: height*np.exp(
+                -(((center_x-x)/width_x)**2+((center_y-y)/width_y)**2)/2)
 
-blobs_log = blob_log(image_gray, max_sigma=30, num_sigma=10, threshold=.1)
+def moments(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution by calculating its
+    moments """
+    total = data.sum()
+    X, Y = np.indices(data.shape)
+    x = (X*data).sum()/total
+    y = (Y*data).sum()/total
+    col = data[:, int(y)]
+    width_x = np.sqrt(np.abs((np.arange(col.size)-y)**2*col).sum()/col.sum())
+    row = data[int(x), :]
+    width_y = np.sqrt(np.abs((np.arange(row.size)-x)**2*row).sum()/row.sum())
+    height = data.max()
+    return height, x, y, width_x, width_y
 
-# Compute radii in the 3rd column.
-blobs_log[:, 2] = blobs_log[:, 2] * sqrt(2)
+def fitgaussian(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution found by a fit"""
+    params = moments(data)
+    errorfunction = lambda p: np.ravel(gaussian(*p)(*np.indices(data.shape)) -
+                                 data)
+    p, success = optimize.leastsq(errorfunction, params)
+    return p
 
-blobs_dog = blob_dog(image_gray, max_sigma=30, threshold=.1)
-blobs_dog[:, 2] = blobs_dog[:, 2] * sqrt(2)
 
-blobs_doh = blob_doh(image_gray, max_sigma=30, threshold=.01)
 
-blobs_list = [blobs_log, blobs_dog, blobs_doh]
-colors = ['yellow', 'lime', 'red']
-titles = ['Laplacian of Gaussian', 'Difference of Gaussian',
-          'Determinant of Hessian']
-sequence = zip(blobs_list, colors, titles)
 
-fig, axes = plt.subplots(1, 3, figsize=(9, 3), sharex=True, sharey=True)
-ax = axes.ravel()
+for image in image_list:
+    image_gray = rgb2gray(image)
 
-for idx, (blobs, color, title) in enumerate(sequence):
-    ax[idx].set_title(title)
-    ax[idx].imshow(image, interpolation='nearest')
-    for blob in blobs:
-        y, x, r = blob
-        c = plt.Circle((x, y), r, color=color, linewidth=2, fill=False)
-        ax[idx].add_patch(c)
-    ax[idx].set_axis_off()
+    blobs = blob_dog(image_gray, max_sigma=30, threshold=.1).astype('uint8')
+    blobs[:, 2] = blobs[:, 2] * sqrt(2)
 
-plt.tight_layout()
-plt.show()
+    print(blobs)
+    print(np.shape(blobs))
+    print (blobs[0])
+    for i in range(np.shape(blobs)[0]):
+        inp = image[blobs[i,:][0]-32:blobs[i,:][0]+32,blobs[i,:][1]-32:blobs[i,:][1]+32,:]
+        print(inp)
+
+        img = np.sum(inp, axis = 2)
+        print(img)
+
+        avg = np.mean(img)
+        var = np.var(img)
+
+        out = img - avg
+        print(out)
+
+        window = (np.less(np.absolute(out), 2*np.ones((64,64))*sqrt(var)) + 0).astype('uint8')
+        print (window)
+        image[i:i+64,j:j+64,:] *= window
+        print ("YAAAAYYYYYY")
